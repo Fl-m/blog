debdfc6c2f6b6c2f0fb67536b713a82d6d0553e8
李林彬 <huanghunjianglin@163.com>
2017-12-11 18:24:56 +0800

    调整

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yz8-5^76%)v0yuc4ldk7kwnc4j8y6w+ze23p#)!g=w3m9o2qe9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost','.ericlee.top']
 
 
 # Application definition
@@ -123,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
